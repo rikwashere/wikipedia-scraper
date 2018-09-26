62d99eb2c613931838f44588a8af0a03eccf62eb
rikwashere <me@rikwassens.com>
2018-09-26 17:57:54 +0200

    Minor edit in output printing

--- db-tools.py
+++ db-tools.py
@@ -42,7 +42,7 @@ def getActiveEditors(df):
 	print '%i unique pages were editted.' % len(df['title'].value_counts().to_dict())
  
 	for edit in edits:
-		print '%s -> %i' % (edit, len(edits[edit]))
+		print '%s edits -> %i' % (edit, len(edits[edit]))
 
 
 def readMongo(collection, query):
