31233a846b715979a5bec521a993962e2a030898
Wassens <R.Wassens@nrc.nl>
2018-09-26 15:35:00 +0200

    Built function that shows heavily editted articles

--- db-tools.py
+++ db-tools.py
@@ -1,10 +1,25 @@
+from collections import Counter
 import pymongo
 
-if __name__ == '__main__':
-	client = pymongo.MongoClient()
-	db = client.wikipedia
+def getStats(db):
 	logs = db.logs
 	revisions = db.revisions
 	db_size = db.command('dbstats')['dataSize'] / (1024*1024)
 
-	print 'Database contains %s logs and %s revisions.\nDatabase is %.2f MB.' % (logs.count(), revisions.count(), db_size)
+	return 'Database contains %s logs and %s revisions.\nDatabase is %.2f MB.' % (logs.count(), revisions.count(), db_size)
+
+def getRevisions(revisions):
+
+	top_edited_pages = Counter([rev['title'] for rev in revisions.find()]).most_common(20)
+
+	for top_edited_page, count in top_edited_pages:
+		print '<%s> was edited %i times.' % (top_edited_pages, count)
+
+		for revision in revisions.find({'title':top_edited_page})
+			print '\t Revision by <%s> at <%s>' % (revision['user'], revision['timestamp'])
+
+if __name__ == '__main__':
+	client = pymongo.MongoClient()
+	db = client.wikipedia
+	
+	print getRevisions(db)
