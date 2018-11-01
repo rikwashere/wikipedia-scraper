c23c1618145b7273dec8969305b349ccdf409db8
rikwashere <me@rikwassens.com>
2018-11-01 16:36:59 +0100

    Added logfiles

--- rev_scraper.py
+++ rev_scraper.py
@@ -15,16 +15,20 @@ params = {	'action' : 'query',
 
 processed = os.listdir('revisions')
 
+def log(txt):
+	with open('logs.txt', 'a') as text_out:
+		text_out.write(txt)
+
 with open('top10k.csv', 'r') as csv_in:
 	titles = csv.DictReader(csv_in)
 
 	for title in titles:
 		if title['page_id'] + '.json' in processed:
-			print 'Already processed <%s>...' % title['page_title']
+			log('Already processed <%s>...' % title['page_title'])
 			continue
 
 		t0 = time.time()
-		print 'Requesting <%s>...' % title['page_title']
+		log('Requesting <%s>...' % title['page_title'])
 
 		params['titles'] = title['page_title']
 		r = requests.get(sauce, params=params)
@@ -32,17 +36,17 @@ with open('top10k.csv', 'r') as csv_in:
 		revisions = json.loads(r.text)
 
 		for page in revisions['query']['pages']:
-			print 'Found %i revisions' % len(revisions['query']['pages'][page]['revisions'])
+			log('Found %i revisions' % len(revisions['query']['pages'][page]['revisions']))
 
 			for rev in revisions['query']['pages'][page]['revisions']:
 				try:
-					print '\t %s: %s' % (rev['timestamp'], rev['user'])
+					log('\t %s: %s' % (rev['timestamp'], rev['user']))
 				except:
-					print '\t Some encoding bullshit.'
+					log('\t Some encoding bullshit.')
 		with open('revisions/%s.json' % title['page_id'], 'w') as json_out:
 			json.dump(revisions['query'], json_out, sort_keys=True, indent=4, encoding='utf8')
 
 		nap_time = time.time() - t0
-		print 'Query completed in %.2f. Sleeping %.2f' % (nap_time, nap_time * 10)
+		log('%s Query completed in %.2f. Sleeping %.2f' % (time.time)nap_time, nap_time * 10)
 
 		time.sleep(nap_time)
