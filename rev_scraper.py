a163f3186319f067c8b552021e027598ac19eb34
rikwashere <me@rikwassens.com>
2018-11-02 15:58:34 +0100

    Changes in scraper.py

--- rev_scraper.py
+++ rev_scraper.py
@@ -4,15 +4,6 @@ import time
 import csv
 import os
 
-sauce = 'https://nl.wikipedia.org/w/api.php?'
-params = {	'action' : 'query',
-			'rvlimit': 'max',
-			'prop': 'revisions',
-			'titles' : '',
-			'rvprop': 'timestamp|user|userid|size|tags|flags|comment',
-			'format': 'json'
-		}
-
 processed = os.listdir('revisions')
 
 def log(txt):
@@ -20,34 +11,47 @@ def log(txt):
 		text_out.write(txt)
 		text_out.write('\n')
 
-with open('top10k.csv', 'r') as csv_in:
-	titles = csv.DictReader(csv_in)
+if __name__ == '__main__':
+	target = raw_input('Load which file?\n> ')
 
-	for title in titles:
-		if title['page_id'] + '.json' in processed:
-			log('Already processed <%s>...' % title['page_title'])
-			continue
+def crawl_revisions(file)
+	sauce = 'https://nl.wikipedia.org/w/api.php?'
+	params = {	'action' : 'query',
+					'rvlimit': 'max',
+					'prop': 'revisions',
+					'titles' : '',
+					'rvprop': 'timestamp|user|userid|size|tags|flags|comment',
+					'format': 'json'
+			}
 
-		t0 = time.time()
-		log('Requesting <%s>...' % title['page_title'])
+	with open(file, 'r') as csv_in:
+		titles = csv.DictReader(csv_in)
 
-		params['titles'] = title['page_title']
-		r = requests.get(sauce, params=params)
+		for title in titles:
+			if title['page_id'] + '.json' in processed:
+				log('Already processed <%s>...' % title['page_title'])
+				continue
 
-		revisions = json.loads(r.text)
+			t0 = time.time()
+			log('Requesting <%s>...' % title['page_title'])
 
-		for page in revisions['query']['pages']:
-			log('Found %i revisions' % len(revisions['query']['pages'][page]['revisions']))
+			params['titles'] = title['page_title']
+			r = requests.get(sauce, params=params)
 
-			for rev in revisions['query']['pages'][page]['revisions']:
-				try:
-					log('\t %s: %s' % (rev['timestamp'], rev['user']))
-				except:
-					log('\t Some encoding bullshit.')
-		with open('revisions/%s.json' % title['page_id'], 'w') as json_out:
-			json.dump(revisions['query'], json_out, sort_keys=True, indent=4, encoding='utf8')
+			revisions = json.loads(r.text)
 
-		nap_time = time.time() - t0
-		log('%s Query completed in %.2f. Sleeping %.2f' % (time.time(), nap_time, nap_time * 10))
+			for page in revisions['query']['pages']:
+				log('Found %i revisions' % len(revisions['query']['pages'][page]['revisions']))
 
-		time.sleep(nap_time)
+				for rev in revisions['query']['pages'][page]['revisions']:
+					try:
+						log('\t %s: %s' % (rev['timestamp'], rev['user']))
+					except:
+						log('\t Some encoding bullshit.')
+			with open('revisions/%s.json' % title['page_id'], 'w') as json_out:
+				json.dump(revisions['query'], json_out, sort_keys=True, indent=4, encoding='utf8')
+
+			nap_time = time.time() - t0
+			log('%s Query completed in %.2f. Sleeping %.2f' % (time.time(), nap_time, nap_time * 10))
+
+			time.sleep(nap_time)
